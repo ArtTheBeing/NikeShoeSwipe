2295730de1e66d9f0ec3ece89ffa4e07c392144c
ArtTheBeing <137590303+ArtTheBeing@users.noreply.github.com>
2023-06-29 14:15:50 -0500

    No Longer Need to run Nikescrape first and fixed results limit bug

--- Nike Price Notifier/main.py
+++ Nike Price Notifier/main.py
@@ -1,9 +1,11 @@
 import pandas as pds
 from interface import ShoeSwiper
-import random
 from nikescrape import NikeScraper
+from pathlib import Path
+path = Path('Nike Price Notifier/data.csv')
 
-
+if path.is_file() == False:  
+    scrape = NikeScraper(3)
 data = pds.read_csv('Nike Price Notifier/data.csv')
 
 nike = ShoeSwiper(data)
--- Nike Price Notifier/nikescrape.py
+++ Nike Price Notifier/nikescrape.py
@@ -11,11 +11,11 @@ class NikeScraper:
         count = 60
         for i in range(times):
             #This recurssion is to fetch the next 60
-            self.url = f'https://api.nike.com/cic/browse/v2?queryid=products&anonymousId=C1A1B7BE17EFE4D2599DFBE5D326287A&country=us&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(US)%26filter%3Dlanguage(en)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(16633190-45e5-4830-a068-232ac7aea82c%2C0f64ecc7-d624-4e91-b171-b83a03dd8550)%26anchor%3D168%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D24&language=en&localizedRangeStr=%7BlowestPrice%7D%20%E2%80%94%20%7BhighestPrice%7D'
-            response = requests.get(self.url)
-            narrowed = response.json()['data']['products']['products']
-            anchor = count
-            count += 60
+            url = f"https://api.nike.com/cic/browse/v2?queryid=products&anonymousId=C1A1B7BE17EFE4D2599DFBE5D326287A&country=us&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(US)%26filter%3Dlanguage(en)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(16633190-45e5-4830-a068-232ac7aea82c%2C0f64ecc7-d624-4e91-b171-b83a03dd8550)%26anchor%3D{anchor}%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D{count}&language=en&localizedRangeStr=%7BlowestPrice%7D%20%E2%80%94%20%7BhighestPrice%7D"
+            response = requests.get(url)
+            if response.status_code == 200:
+                narrowed = response.json()['data']['products']['products']
+            anchor += count  # Update anchor for the next iteration
             for i in range(len(narrowed)):
 
                 #Data Cleaning and Data Forming
@@ -27,10 +27,8 @@ class NikeScraper:
                 url = f'nike.com{segment}'
                 data = {'Name': name, 'CurrentPrice': currentp, 'MaxPrice': fullp, 'image-link' : image, 'url': url}
                 self.df.append(data)
-    
+
+
     def to_dataframe(self):
         df = pds.DataFrame(self.df)
         df.to_csv('Nike Price Notifier/data.csv', index= False)
-
-
-scrape = NikeScraper(5)
